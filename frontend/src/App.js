1704ef1ac4635a8f92170daba041ab045bb05393
Quincy Thibodeaux <qthibgit@gmail.com>
2020-10-03 13:24:11 -0500

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactComponent from './ReactComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React Full Stack Skeleton
+        <ReactComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
